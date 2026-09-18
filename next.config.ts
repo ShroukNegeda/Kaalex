6711e343ff5763a59edb1959c320e71431f4a29c
ShroukNegeda <sh.ta.3366@gmail.com>
2026-09-18 16:14:02 +0300

    Update Kaalex project

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1600, 2000],
+    deviceSizes: [384, 480, 640, 750, 828, 1080, 1200, 1600, 2000],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     remotePatterns: [
       { protocol: "https", hostname: "i.pravatar.cc" },
